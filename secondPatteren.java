5894c46a334cee4e4148d20241558acaf9d8bac8
Sumon224 <sumon6342@gmail.com>
2022-08-17 22:01:59 +0600

    Initial commit

---
+++ secondPatteren.java
@@ -0,0 +1,21 @@
+public class secondPatteren {
+    //Hollow Rectangle
+    public static void main(String[] args) {
+        int n = 4;
+        int m = 5;
+        int i;
+        int j;
+
+        for(i=1; i<=n; i++){
+            for (j=1; j<=m; j++){
+                if(i == 1 || j == 1 || i == n || j ==m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+}
